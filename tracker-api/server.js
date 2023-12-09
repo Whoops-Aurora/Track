ecb1b61bd0ac43aeb26cf1afa9c148324e666e96
Adwait S <67193262+Whoops-Aurora@users.noreply.github.com>
2023-12-09 12:15:20 +0530

    "first commit"

---
+++ tracker-api/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+
+const mongoose = require('mongoose');  
+const uri = process.env.ATLAS_URI;  //database uri
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });  //connect to database
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully")
+})
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());  //middleware
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
