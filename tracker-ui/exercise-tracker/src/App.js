7ff92d0e5b87b176ca489ba45cceffab562778f6
Adwait S <67193262+Whoops-Aurora@users.noreply.github.com>
2023-12-10 10:20:55 +0530

    "added ui"

--- tracker-ui/exercise-tracker/src/App.js
+++ tracker-ui/exercise-tracker/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo2.svg';
 import './App.css';
-
+import "bootstrap/dist/css/bootstrap.min.css"
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Exercises from './components/Exercises.component.js';
+import CreateExercises from './components/CreateExercises.component.js';
+import Navbar from "./components/Navbar.js"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Exercises />} />
+        {/* <Route path="/edit/:id" element={<EditExercises />} /> */}
+        <Route path="/create" element={<CreateExercises />} />
+        {/* <Route path="/user" element={<CreateUsers />} /> */}
+        </Routes>
+      </Router>
+    </>
   );
 }
 
